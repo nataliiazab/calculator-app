19f6d6adb5324512268b432906d5baf8054ac123
nataliizab <nataliia.zab@gmail.com>
2023-04-28 23:31:03 +0100

    pre-final css

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
+const getStyleName = (btn) => {
+  const className = {
+    "=": "equals",
+    "x": "operation",
+    "/": "operation",
+    "-": "operation",
+    "+": "operation",
+    "C": "upper-btn",
+    "+/-": "upper-btn",
+    "%": "upper-btn"
+  };
+
+  return className[btn];
+};
 
 const Button = ({ value }) => {
-  return <button className="button">{value}</button>;
+  return <button className={`${getStyleName(value)} button`}>{value}</button>;
 };
 
 export default Button;
