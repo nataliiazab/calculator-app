c976d92d7968c50eaccac3f00f658e2aff27220d
mohamedabdi <alaale7404@gmail.com>
2023-05-01 17:25:38 +0100

    Added operators (*/+-%), "delete", "=", AC and "."

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,13 @@ import React, { useContext } from "react";
 import { CalcContext } from "../context/CalcContext";
 
 const getStyleName = (btn) => {
-   const className = {
+  const className = {
     "=": "equals",
     x: "operation",
     "/": "operation",
     "-": "operation",
     "+": "operation",
-    C: "upper-btn",
+    " C": "upper-btn",
     "+/-": "upper-btn",
     "%": "upper-btn",
   };
@@ -17,47 +17,106 @@ const getStyleName = (btn) => {
 };
 
 const Button = ({ value }) => {
-  const { calc,setCalc } = useContext(CalcContext);
-
+  const { calc, setCalc } = useContext(CalcContext);
+  // User Click comma
   const commaClick = () => {
     setCalc({
       ...calc,
-      num: 89,
-      // num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
-    })
-  }
+      // num: 89,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+  // User click C
 
   const resetClick = () => {
-    setCalc({sign: '', num: 0, res: 0})
-  }
+    setCalc({ sign: "", num: 0, res: 0 });
+  };
+  // User click number
 
   const handleClickButton = () => {
-    const numberString = value.toString()
+    const numberString = value.toString();
 
     let numberValue;
+    if (numberString === "0" && calc.num === 0) {
+      numberValue = "0";
+    } else {
+      numberValue = Number(calc.num + numberString);
+    }
 
     setCalc({
       ...calc,
-      num: numberValue
-    })
-  }
-
-  const handleBtnClick = () => {
-
-    const results = {
-      '.': commaClick,
-      'C': resetClick
+      num: numberValue,
+    });
+  };
+  // User click operation
+  const signClick = () => {
+    setCalc({
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+  // USer click equals
+  const equalsClick = () => {
+    if (calc.res && calc.num) {
+      const math = (a, b, sign) => {
+        const result = {
+          "+": (a, b) => a + b,
+          "-": (a, b) => a - b,
+          x: (a, b) => a * b,
+          "/": (a, b) => a / b,
+        };
+        return result[sign](a, b);
+      };
+      setCalc({
+        res: math(calc.res, calc.num, calc.sign),
+        sign: "",
+        num: 0,
+      });
     }
+  };
 
-    if(results[value]) {
-      return results[value]()
-    } else{
-      return handleClickButton()
+  // User Click persen
+  const persenClick = () => {
+    setCalc({
+      num: calc.num / 100,
+      res: calc.res / 100,
+      sign: "",
+    });
+  };
+  // User click invert button
+  const invertClick = () => {
+    setCalc({
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: "",
+    });
+  };
+  const handleBtnClick = () => {
+    const results = {
+      ".": commaClick,
+      C: resetClick,
+      "/": signClick,
+      x: signClick,
+      "-": signClick,
+      "+": signClick,
+      "=": equalsClick,
+      "%": persenClick,
+      "+/-": invertClick,
+    };
+
+    if (results[value]) {
+      return results[value]();
+    } else {
+      return handleClickButton();
     }
   };
 
   return (
-    <button onClick={handleBtnClick} className={`${getStyleName(value)} button`}>
+    <button
+      onClick={handleBtnClick}
+      className={`${getStyleName(value)} button`}
+    >
       {value}
     </button>
   );
